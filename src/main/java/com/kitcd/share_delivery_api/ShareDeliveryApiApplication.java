90b4f45637f8b26d0bca7907b45875df97d17405
hyunkim <68850889+aittaa@users.noreply.github.com>
2022-04-12 21:20:45 +0900

    Initial commit

---
+++ src/main/java/com/kitcd/share_delivery_api/ShareDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.kitcd.share_delivery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitcd/share_delivery_api/ShareDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitcd.share_delivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
