e8a3ac84473d0594e94de5ed0635d98f089a3a1f
hyunkim <han.kim@kakao.com>
2022-04-13 19:50:35 +0900

    Add missing domain classes

---
+++ src/main/java/com/kitcd/share_delivery_api/domain/blockedFriend/BlockedFriendRepository.java
@@ -0,0 +1,7 @@
+package com.kitcd.share_delivery_api.domain.blockedFriend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockedFriendRepository extends JpaRepository<BlockedFriend, Long> {
+
+}
