75260bc8ac9e529956009d33fd9fa987bae8aa41
hyunkim <han.kim@kakao.com>
2022-04-17 21:00:53 +0900

    Feat 미 인증, 인가예외 handler

--- src/main/java/com/kitcd/share_delivery_api/security/configs/SecurityConfig.java
+++ src/main/java/com/kitcd/share_delivery_api/security/configs/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.kitcd.share_delivery_api.security.configs;
 
 
 import com.kitcd.share_delivery_api.security.filter.JsonLoginProcessingFilter;
+import com.kitcd.share_delivery_api.security.handler.JsonAccessDeniedHandler;
 import com.kitcd.share_delivery_api.security.handler.JsonAuthFailureHandler;
 import com.kitcd.share_delivery_api.security.handler.JsonAuthSuccessHandler;
+import com.kitcd.share_delivery_api.security.handler.JsonLoginAuthEntryPoint;
 import com.kitcd.share_delivery_api.security.provider.JsonAuthProvider;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +20,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -36,6 +40,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationFailureHandler authenticationFailureHandler(){
         return new JsonAuthFailureHandler();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint jsonLoginAuthEntryPoint() {
+        return new JsonLoginAuthEntryPoint();
+    }
+
+    @Bean
+    public AccessDeniedHandler jsonAccessDeniedHandler() {
+        return new JsonAccessDeniedHandler();
     }
 
     @Bean
@@ -67,18 +81,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .antMatcher("/api/**") // `api/` 이하 URL에 한해서 설정 클래스 동작
                 .authorizeRequests()
-                .antMatchers("/api/messages").hasRole("MANAGER")
                 .antMatchers("/api/accounts").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterAt(jsonLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
 
 
+        /* 미인증, 인가예외 핸들러 등록 */
+        http
+                .exceptionHandling()
+                .accessDeniedHandler(jsonAccessDeniedHandler())
+                .authenticationEntryPoint(jsonLoginAuthEntryPoint())
+        ;
+
         http.csrf().disable(); // 스프링 시큐리티 기본 보안 설정 상 post 요청 시에 csrf 토큰 요구하기에 비활성화 해주어야 한다.
 
     }
+
+
 
 }
---
+++ src/main/java/com/kitcd/share_delivery_api/security/handler/JsonLoginAuthEntryPoint.java
@@ -0,0 +1,20 @@
+package com.kitcd.share_delivery_api.security.handler;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JsonLoginAuthEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException e) throws IOException {
+
+        res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UnAuthorized");
+
+    }
+
+}
