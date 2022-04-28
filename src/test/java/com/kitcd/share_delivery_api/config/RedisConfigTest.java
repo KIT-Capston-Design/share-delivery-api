32e38d8ba85e243c907c77d2f5322614a64f015a
hyunkim <han.kim@kakao.com>
2022-04-28 15:32:12 +0900

    Add spring data redis 관련 의존성, 기본 레디스 세팅

---
+++ src/test/java/com/kitcd/share_delivery_api/config/RedisConfigTest.java
@@ -0,0 +1,32 @@
+package com.kitcd.share_delivery_api.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class RedisConfigTest {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Test
+    @DisplayName("Redis String Value Write and Read")
+    void testStrings() {
+        // given
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        String key = "stringKey";
+
+        // when
+        valueOperations.set(key, "hello");
+
+        // then
+        String value = valueOperations.get(key);
+        assertThat(value).isEqualTo("hello");
+    }
+
+}
