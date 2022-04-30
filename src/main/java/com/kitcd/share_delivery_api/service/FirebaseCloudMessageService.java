7b45e42fb4d86d703e67451998744f11a6de90a6
hyunkim <han.kim@kakao.com>
2022-04-30 16:00:04 +0900

    Chore FCM 위한 firebase-admin 의존성 추가 ...
    - FCM 서비스 틀 생성

---
+++ src/main/java/com/kitcd/share_delivery_api/service/FirebaseCloudMessageService.java
@@ -0,0 +1,4 @@
+package com.kitcd.share_delivery_api.service;
+
+public interface FirebaseCloudMessageService {
+}
---
+++ src/main/java/com/kitcd/share_delivery_api/service/impl/FirebaseCloudMessageServiceImpl.java
@@ -0,0 +1,13 @@
+package com.kitcd.share_delivery_api.service.impl;
+
+import com.kitcd.share_delivery_api.service.FirebaseCloudMessageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FirebaseCloudMessageServiceImpl implements FirebaseCloudMessageService {
+
+}
