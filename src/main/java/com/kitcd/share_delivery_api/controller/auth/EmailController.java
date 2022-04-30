a2f9c7659801d3a3b0b538930feb5d6f1c35de6e
kimhyeonjin <khj745700@naver.com>
2022-05-01 01:00:24 +0900

    fix 이메일 http 헤더 수정

--- src/main/java/com/kitcd/share_delivery_api/controller/auth/EmailController.java
+++ src/main/java/com/kitcd/share_delivery_api/controller/auth/EmailController.java
@@ -34,7 +34,7 @@ public class EmailController {
             emailService.sendEmail(email);
             return ResponseEntity.status(HttpStatus.ACCEPTED).body("성공적으로 인증메일이 전송되었습니다.");
         }catch (EntityExistsException e){
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("이메일이 존재합니다.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이메일이 존재합니다.");
         }
     }
 
@@ -44,7 +44,7 @@ public class EmailController {
             emailService.AccountAddEmail(getAccount().getPhoneNumber(), accountEmailAddDTO.getEmail());
             return ResponseEntity.status(HttpStatus.ACCEPTED).body("성공적으로 이메일이 등록되었습니다.");
         }
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("인증번호가 일치하지 않습니다.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("인증번호가 일치하지 않습니다.");
     }
 
     private Account getAccount() {
