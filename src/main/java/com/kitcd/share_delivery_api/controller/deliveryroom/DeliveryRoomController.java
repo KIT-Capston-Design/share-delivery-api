3848d71a7ba7223cc96905f2a03437e3c814f855
kimhyeonjin <khj745700@naver.com>
2022-05-02 20:44:30 +0900

    Refactor 현재 세션에 접속한 유저의 정보 메서드 정적화

--- src/main/java/com/kitcd/share_delivery_api/controller/deliveryroom/DeliveryRoomController.java
+++ src/main/java/com/kitcd/share_delivery_api/controller/deliveryroom/DeliveryRoomController.java
@@ -7,15 +7,19 @@ import com.kitcd.share_delivery_api.service.DeliveryRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.kitcd.share_delivery_api.controller.common.CurrentLoggedInSession.getAccount;
+
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Validated
 public class DeliveryRoomController {
 
     private final DeliveryRoomService deliveryRoomService;
@@ -25,9 +29,4 @@ public class DeliveryRoomController {
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
-    private Account getAccount() {
-        AccountContext userData = (AccountContext) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Account user = userData.getAccount();
-        return user;
-    }
 }
