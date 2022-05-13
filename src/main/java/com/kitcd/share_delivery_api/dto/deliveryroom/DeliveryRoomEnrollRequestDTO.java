2033f318dcb565700ed585b3872d6594cf83d2bc
kimhyeonjin <khj745700@naver.com>
2022-05-14 02:22:07 +0900

    Add UrlParserFindStoreName(~ing)

--- src/main/java/com/kitcd/share_delivery_api/dto/deliveryroom/DeliveryRoomEnrollRequestDTO.java
+++ src/main/java/com/kitcd/share_delivery_api/dto/deliveryroom/DeliveryRoomEnrollRequestDTO.java
@@ -4,12 +4,11 @@ import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.PlatformType;
 import com.kitcd.share_delivery_api.domain.jpa.account.Account;
 import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.DeliveryRoom;
 import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.DeliveryRoomState;
-import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.PlatformType;
 import com.kitcd.share_delivery_api.domain.jpa.receivinglocation.ReceivingLocation;
 import com.kitcd.share_delivery_api.domain.jpa.storecategory.StoreCategory;
 import com.kitcd.share_delivery_api.dto.ordermenu.OptionMenuRequestDTO;
 import com.kitcd.share_delivery_api.dto.ordermenu.OrderMenuRequestDTO;
-import com.kitcd.share_delivery_api.dto.receivinglocation.ReceivingLocationDTO;
+import com.kitcd.share_delivery_api.utils.url.UrlParserFindStoreName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -36,13 +35,17 @@ public class DeliveryRoomEnrollRequestDTO {
     private PlatformType linkPlatformType;
 
     public DeliveryRoom toEntity(Account leader, ReceivingLocation location, StoreCategory storeCategory){
+        UrlParserFindStoreName urlParserFindStoreName = new UrlParserFindStoreName(shareStoreLink, linkPlatformType);
         DeliveryRoom room = DeliveryRoom.builder()
                 .content(this.getContent())
                 .receivingLocation(location)
                 .leader(leader)
                 .limitPerson(this.getLimitPerson())
                 .linkPlatformType(this.getLinkPlatformType())
-                .status(DeliveryRoomState.WAITING)
+                .status(DeliveryRoomState.OPEN)
+                .storeName(urlParserFindStoreName.getStoreName())
+                .storeLink(urlParserFindStoreName.getUrl())
+                .status(DeliveryRoomState.OPEN)
                 .storeCategory(storeCategory)
                 .build();
         return room;
