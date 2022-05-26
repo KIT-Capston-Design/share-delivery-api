a62bd296ee4f4a8b3332d4b5548668dd8203d911
kimhyeonjin <khj745700@naver.com>
2022-05-26 20:31:47 +0900

    Chore DeliveryRoomEnroll Response 반환

--- src/main/java/com/kitcd/share_delivery_api/dto/deliveryroom/DeliveryRoomEnrollResponseDTO.java
+++ src/main/java/com/kitcd/share_delivery_api/dto/deliveryroom/DeliveryRoomEnrollResponseDTO.java
@@ -17,38 +17,4 @@ import java.util.List;
 @Builder
 public class DeliveryRoomEnrollResponseDTO {
     private Long roomId;
-    private String content;
-    @NotBlank
-    private Long receivingLocationId;
-    private Long limitPerson;
-    private String storeCategory;
-    private Long deliveryTip;
-    private List<MenuResDTO> menuList;
-    private ShareStoreDTO shareStore;
-
-    public DeliveryRoomEnrollResponseDTO parseDTO(List<OrderMenu> orderMenus, DeliveryRoom room){
-        return DeliveryRoomEnrollResponseDTO.builder()
-                .roomId(room.getDeliveryRoomId())
-                .deliveryTip(room.getEstimatedDeliveryTip())
-                .content(room.getContent())
-                .receivingLocationId(room.getReceivingLocation().getReceivingLocationId())
-                .limitPerson(room.getLimitPerson())
-                .storeCategory(room.getStoreCategory().getCategoryName())
-                .menuList(MenuResDTO.parseEntityListToDTO(orderMenus))
-                .shareStore(ShareStoreDTO.builder().name(room.getStoreName())
-                        .link(room.getStoreLink())
-                        .type(room.getLinkPlatformType())
-                        .build())
-                .build();
-    }
-
-    @AllArgsConstructor
-    @Getter
-    @NoArgsConstructor
-    @Builder
-    private static class ShareStoreDTO{
-        private String name;
-        private String link;
-        private PlatformType type;
-    }
 }
