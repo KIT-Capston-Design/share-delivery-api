6a6dd12f4a317db9785d05ec3347e79ff63e9d00
hyunkim <68850889+aittaa@users.noreply.github.com>
2022-06-04 18:28:12 +0900

    Fix 주도자 배달 주문 정보 등록(B)
    - Refactor 코드 일부 서비스 계층으로 이동
    - Fix 주도자가 지불한 배달비 금액을 받지 않고 예상 배달비 값을 저장하는 문제
    - Update DummyDataLoader 1L DeliveryRomm Entity WAITING_PAYMENT 상태로

--- src/main/java/com/kitcd/share_delivery_api/service/PaymentService.java
+++ src/main/java/com/kitcd/share_delivery_api/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.kitcd.share_delivery_api.service;
 
 import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.DeliveryRoom;
+import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.DeliveryRoomState;
 import com.kitcd.share_delivery_api.domain.jpa.payment.Payment;
 import com.kitcd.share_delivery_api.dto.payment.PaymentEnrollRequestDTO;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,6 @@ import java.util.List;
 
 public interface PaymentService {
 
-    void enrollPayment(PaymentEnrollRequestDTO dto, List<MultipartFile> images, DeliveryRoom room);
+    DeliveryRoomState enrollPayment(PaymentEnrollRequestDTO dto, List<MultipartFile> images, DeliveryRoom room);
 
 }
--- src/main/java/com/kitcd/share_delivery_api/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/kitcd/share_delivery_api/service/impl/PaymentServiceImpl.java
@@ -1,26 +1,23 @@
 package com.kitcd.share_delivery_api.service.impl;
 
 import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.DeliveryRoom;
-import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.DeliveryRoomRepository;
+import com.kitcd.share_delivery_api.domain.jpa.deliveryroom.DeliveryRoomState;
 import com.kitcd.share_delivery_api.domain.jpa.payment.Payment;
 import com.kitcd.share_delivery_api.domain.jpa.payment.PaymentRepository;
-import com.kitcd.share_delivery_api.domain.jpa.paymentdiscount.PaymentDiscount;
-import com.kitcd.share_delivery_api.domain.jpa.paymentorderform.PaymentOrderForm;
 import com.kitcd.share_delivery_api.domain.redis.deliveryroom.ActivatedDeliveryRoomInfo;
 import com.kitcd.share_delivery_api.domain.redis.deliveryroom.ActivatedDeliveryRoomInfoRedisRepository;
 import com.kitcd.share_delivery_api.dto.fcm.FCMDataType;
 import com.kitcd.share_delivery_api.dto.payment.PaymentEnrollRequestDTO;
+import com.kitcd.share_delivery_api.dto.paymentdiscount.PaymentDiscountEnrollRequestDTO;
 import com.kitcd.share_delivery_api.service.*;
+import com.kitcd.share_delivery_api.utils.ContextHolder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Transactional
@@ -38,29 +35,48 @@ public class PaymentServiceImpl implements PaymentService {
     private final ActivatedDeliveryRoomInfoRedisRepository activatedDeliveryRoomInfoRedisRepository;
 
     private final FirebaseCloudMessageService firebaseCloudMessageService;
+    private final RemittanceService remittanceService;
 
     @Override
-    public void enrollPayment(PaymentEnrollRequestDTO dto, List<MultipartFile> images, DeliveryRoom room) {
+    public DeliveryRoomState enrollPayment(PaymentEnrollRequestDTO dto, List<MultipartFile> images, DeliveryRoom room) {
 
+        //방장인지 체크
+        room.checkLeader(ContextHolder.getAccountId());
+
+        room.enrollPaymentStatusCheck();
 
         Payment payment = paymentRepository.save(dto.toEntity(room));
 
-        paymentDiscountService.enrollPaymentDiscount(dto.getDiscounts(), payment);
+        List<PaymentDiscountEnrollRequestDTO> discounts = dto.getDiscounts();
 
-        paymentOrderFormService.enrollPaymentOrderForm(images,payment);
+        paymentDiscountService.enrollPaymentDiscount(discounts, payment);
+
+        paymentOrderFormService.enrollPaymentOrderForm(images, payment);
+
+
+        // 총 할인 금액
+        long totalDiscountAmount = discounts.stream().map(PaymentDiscountEnrollRequestDTO::getAmount).mapToLong(Long::longValue).sum();
+
+        //Remittance Entity 생성
+        remittanceService.createRemittanceEntities(room, payment, totalDiscountAmount);
+
 
         //그룹키 가져오고
+
         ActivatedDeliveryRoomInfo activatedDeliveryRoomInfo = activatedDeliveryRoomInfoRedisRepository.findByDeliveryRoomId(room.getDeliveryRoomId());
 
         //전송할 메시지 생성
         HashMap<String, Object> data = new HashMap<>();
         data.put("type", FCMDataType.COMPLETED_ORDER);
         data.put("roomId", room.getDeliveryRoomId());
+
         //전송
         firebaseCloudMessageService.sendMessageTo(
                 activatedDeliveryRoomInfo.getFcmGroupToken(),
                 room.getContent() + "배달 주문 정보가 등록되었습니다.",
                 "null",
                 data);
+
+        return room.getStatus();
     }
 }
