a1bc0e4277a64c98adb19a474b55a1ac2b8e3f32
hyunkim <68850889+aittaa@users.noreply.github.com>
2022-06-06 13:15:05 +0900

    Fix 업로드 파일 경로 매핑

--- src/main/java/com/kitcd/share_delivery_api/configs/WebConfig.java
+++ src/main/java/com/kitcd/share_delivery_api/configs/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**")
-                .addResourceLocations("file://" + baseDir);
+                .addResourceLocations(baseDir);
     }
 }
